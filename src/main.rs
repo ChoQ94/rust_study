2922e68b91bf28ea5a1cf180f388dd7e6e37cdbf
choq <whrbdnjs1994@gmail.com>
2022-02-16 21:56:05 +0900

    feat: code block sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 
 
 fn main() {
-    // 이게 코멘트죠!
-    println!("Hellog, world!");
+    // let x = 9; // 관심없음 => 쉐도윙
+    // let x = double(x);
+    // let x = triple(x);
+    // println!("{} ㅋㅋㅋ",my_number);
+
+    //코드블럭!
+    let my = 9;
+    {
+        let my = "zzz";
+        println!("{}", my)
+    }
+    println!("{}", my)
+    
 }
