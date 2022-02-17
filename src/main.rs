9117b8b8b95d22a9818ac49344b15cd63b791fc4
choq <whrbdnjs1994@gmail.com>
2022-02-17 22:00:54 +0900

    feat: lecture 5 main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 
 
 fn main() {
+
+    ////////// 2/17
+    println!("size of a char: {}", std::mem::size_of::<char>()); //4bytes
+    println!("size of a a: {}", "a".len()); //1
+    println!("size of a aaa: {}", "aaa".len());
+
+    let slice = "Hello world! 안녕";
+    println!("Slice is {} bytes and {} character", slice.len(), slice.chars().count());
+
+
+
+
+    ////////// 2/16
     // let x = 9; // 관심없음 => 쉐도윙
     // let x = double(x);
     // let x = triple(x);
     // println!("{} ㅋㅋㅋ",my_number);
 
     //코드블럭!
-    let my = 9;
-    {
-        let my = "zzz";
-        println!("{}", my)
-    }
-    println!("{}", my)
+    // let my = 9;
+    // {
+    //     let my = "zzz";
+    //     println!("{}", my)
+    // }
+    // println!("{}", my)
     
 }
