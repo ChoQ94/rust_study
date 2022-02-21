7c238f12882d2cec568274cee4d611aa5af751db
choq <whrbdnjs1994@gmail.com>
2022-02-21 22:00:39 +0900

    feat: 2/21

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 
 
-    
+     /// 10/10
     ////////// 2/17
     // println!("size of a char: {}", std::mem::size_of::<char>()); //4bytes
     // println!("size of a a: {}", "a".len()); //1
